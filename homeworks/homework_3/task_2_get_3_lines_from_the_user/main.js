8ec5e1b92c6bead828a4cfe49e2fa6c8fc1370c8
Anna <anna.vasiljeva94@gmail.com>
2025-01-07 22:43:40 +0100

    add task2 to homework3

---
+++ homeworks/homework_3/task_2_get_3_lines_from_the_user/main.js
@@ -0,0 +1,5 @@
+const surname = prompt("Будь ласка, введіть Ваше прізвище:");
+const firstName = prompt("Будь ласка, введіть Ваше ім'я:");
+const patronymic = prompt("Будь ласка, введіть Ваше по батькові:");
+const greeting = `Вітаємо, ${surname} ${firstName} ${patronymic}!`;
+alert(greeting);
